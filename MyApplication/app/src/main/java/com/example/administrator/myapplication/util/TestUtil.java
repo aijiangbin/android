a2807cae755634cb2d52dea7fd53867fb7f2fa1a
aijiangbin <jiangbinai@irissz.com>
2018-01-04 11:50:31 +0800

    添加一个 工具类

---
+++ MyApplication/app/src/main/java/com/example/administrator/myapplication/util/TestUtil.java
@@ -0,0 +1,11 @@
+package com.example.administrator.myapplication.util;
+
+/**
+ * Created by Administrator on 2018/1/4.
+ */
+
+public class TestUtil {
+    public  static  void   add (){
+        
+    }
+}
